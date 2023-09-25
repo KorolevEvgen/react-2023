14642576bd3a6529aac193d506d5968445608cae
Korolev <korolev.evgen163@gmail.com>
2023-09-25 17:02:58 +0300

    lesson1

---
+++ src/components/users/Users.js
@@ -0,0 +1,18 @@
+import React, {useState} from 'react';
+import User from '../user/User';
+
+export default function Users() {
+    let [users,setUsers] = useState([]);      // функція яка викликає масив [get,set()]
+                                                        // get - обєкт, set() - функція
+
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(value => value.json())
+        .then(value => {
+            setUsers(value);
+        });
+    return (
+        <div>
+            {users.map(user => (<User item={user} key={user.id}/>))}
+        </div>
+    );
+};
